39f3fca82cdcfb3c32ccfdb458d95dd71323466e
mirekwalczak <miroslaw.walczak@gmail.com>
2018-11-14 10:19:38 +0100

    Add CORS

--- main.go
+++ main.go
@@ -11,8 +11,12 @@ import (
 func main() {
 	http.HandleFunc("/", Info)
 	http.HandleFunc("/api/v1/content/main/", MainContent)
-	log.Println("Listening on port", os.Getenv("PORT"))
-	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
+	log.Println("Listening on port", port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
 		log.Printf("error: listing and serving: %s", err)
 		return
 	}
@@ -42,6 +46,7 @@ You should definitelly attend!
 We are waiting for you!`,
 	}
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	if err := json.NewEncoder(w).Encode(content); err != nil {
 		log.Printf("error: encoding response: %s", err)
 	}
