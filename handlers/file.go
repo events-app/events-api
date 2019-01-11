5e6f51ae1f1648b09414e54c388617762a6d7a16
mirekwalczak <miroslaw.walczak@gmail.com>
2019-01-11 11:34:16 +0100

    return JSON afer file upload

--- handlers/file.go
+++ handlers/file.go
@@ -21,20 +21,39 @@ func UploadFile(uploadPath string, maxUploadSize int64) http.HandlerFunc {
 		}
 
 		// parse and validate file
-		f, _, err := r.FormFile("file")
+		f, header, err := r.FormFile("file")
 		if err != nil {
 			web.ErrorJSON(w, "invalid file", http.StatusBadRequest)
 			return
 		}
+		realName := header.Filename
 		defer f.Close()
-		filename, err := file.Upload(f, uploadPath)
+		file, err := file.Upload(f, uploadPath, realName)
 		if err != nil {
 			web.ErrorJSON(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		fl := file.New(fmt.Sprintf("https://%s/files/%s", r.Host, filename))
+		file.Path = fmt.Sprintf("https://%s/files/%s", r.Host, file.Path)
+		// fl := file.New(fmt.Sprintf("https://%s/files/%s", r.Host, file.Name))
 		// f := file.File{Path: fmt.Sprintf("https://%s/files/%s", r.Host, filename)}
-		if err := json.NewEncoder(w).Encode(&fl); err != nil {
+		if err := json.NewEncoder(w).Encode(&file); err != nil {
+			log.Printf("error: encoding response: %s", err)
+		}
+	})
+}
+func GetFiles(path string) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		files, err := file.ReadDir(path)
+		if err != nil {
+			web.ErrorJSON(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		// if files == nil {
+		// 	web.ErrorJSON(w, "no files", http.StatusNoContent)
+		// 	return
+		// }
+		// zrobic mapowanie do jakis struktury
+		if err := json.NewEncoder(w).Encode(&files); err != nil {
 			log.Printf("error: encoding response: %s", err)
 		}
 	})
--- internal/file/file.go
+++ internal/file/file.go
@@ -13,19 +13,15 @@ import (
 )
 
 type File struct {
+	Name string `json:"name"`
 	Path string `json:"path"`
 }
 
-func New(path string) *File {
-	return &File{path}
-}
-
 // Upload handles uploading files like png, jpg, etc to server
-func Upload(file io.Reader, path string) (string, error) {
+func Upload(file io.Reader, path string, realName string) (*File, error) {
 	fileBytes, err := ioutil.ReadAll(file)
 	if err != nil {
-		// renderError(w, "INVALID_FILE", http.StatusBadRequest)
-		return "", errors.New("invalid file")
+		return nil, errors.New("invalid file")
 	}
 
 	// check file type, detectcontenttype only needs the first 512 bytes
@@ -36,34 +32,41 @@ func Upload(file io.Reader, path string) (string, error) {
 	case "application/pdf":
 		break
 	default:
-		// renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
-		return "", errors.New("invalid file type")
+		return nil, errors.New("invalid file type")
 	}
 	fileName := randToken(12)
 	fileEndings, err := mime.ExtensionsByType(filetype)
 	if err != nil {
-		// renderError(w, "CANT_READ_FILE_TYPE", http.StatusInternalServerError)
-		return "", errors.New("cannot read file type")
+		return nil, errors.New("cannot read file type")
 	}
 	newPath := filepath.Join(path, fileName+fileEndings[0])
-	// fmt.Printf("FileType: %s, File: %s\n", fileType, newPath)
 
 	// write file
 	newFile, err := os.Create(newPath)
 	if err != nil {
-		// renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
-		return "", errors.New("cannot write file")
+		return nil, errors.New("cannot write file")
 	}
 	defer newFile.Close() // idempotent, okay to call twice
 	if _, err := newFile.Write(fileBytes); err != nil || newFile.Close() != nil {
-		// renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
-		return "", errors.New("canot write file")
+		return nil, errors.New("canot write file")
 	}
-	return fileName + fileEndings[0], nil
+	return &File{realName, fileName + fileEndings[0]}, nil
 }
 
 func randToken(len int) string {
 	b := make([]byte, len)
 	rand.Read(b)
 	return fmt.Sprintf("%x", b)
+}
+
+// ReadDir returns list of files in a given path
+func ReadDir(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed opening directory: %s", err)
+	}
+	defer file.Close()
+
+	list, _ := file.Readdirnames(0) // 0 to read all files and folders
+	return list, nil
 }
