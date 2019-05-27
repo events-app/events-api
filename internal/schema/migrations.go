42aef71ffce81d390f015834f4551fd60eaebd70
mirekwalczak <miroslaw.walczak@ing.com>
2019-05-27 11:07:54 +0200

    Fix migrations

--- internal/schema/migrations.go
+++ internal/schema/migrations.go
@@ -14,25 +14,11 @@ var migrations = []darwin.Migration{
 		Version:     1,
 		Description: "Add card, menu, file, user",
 		Script: `
-		BEGIN;
-		--
-		-- Create model Card
-		--
-		CREATE TABLE "card" ("id" serial NOT NULL PRIMARY KEY, "name" varchar(200) NOT NULL, "text" text NOT NULL, "created" timestamp NOT NULL, "updated" timestamp NOT NULL);
-		--
-		-- Create model File
-		--
-		CREATE TABLE "file" ("id" serial NOT NULL PRIMARY KEY, "name" varchar(200) NOT NULL, "path" varchar(500) NOT NULL, "created" timestamp NOT NULL, "updated" timestamp NOT NULL);
-		--
-		-- Create model User
-		--
-		CREATE TABLE "user" ("id" serial NOT NULL PRIMARY KEY, "username" varchar(200) NOT NULL, "password" varchar(100) NOT NULL, "role" varchar(100) NOT NULL, "created" timestamp NOT NULL, "updated" timestamp NOT NULL);
-		--
-		-- Create model Menu
-		--
-		CREATE TABLE "menu" ("id" serial NOT NULL PRIMARY KEY, "name" varchar(200) NOT NULL, "created" timestamp NOT NULL, "updated" timestamp NOT NULL, "card_id" integer NOT NULL);
+		CREATE TABLE "card" ("id" serial NOT NULL PRIMARY KEY, "name" varchar(200) NOT NULL, "text" text NOT NULL, "created" timestamp NOT NULL, "updated" timestamp);
+		CREATE TABLE "file" ("id" serial NOT NULL PRIMARY KEY, "name" varchar(200) NOT NULL, "path" varchar(500) NOT NULL, "created" timestamp NOT NULL, "updated" timestamp);
+		CREATE TABLE "user" ("id" serial NOT NULL PRIMARY KEY, "username" varchar(200) NOT NULL, "password" varchar(100) NOT NULL, "role" varchar(100) NOT NULL, "created" timestamp NOT NULL, "updated" timestamp);
+		CREATE TABLE "menu" ("id" serial NOT NULL PRIMARY KEY, "name" varchar(200) NOT NULL, "created" timestamp NOT NULL, "updated" timestamp, "card_id" integer NOT NULL);
 		ALTER TABLE "menu" ADD CONSTRAINT "menu_card_id_7cfd48a2_fk_card_id" FOREIGN KEY ("card_id") REFERENCES "card" ("id") DEFERRABLE INITIALLY DEFERRED;
-		CREATE INDEX "menu_card_id_7cfd48a2" ON "menu" ("card_id");
-		COMMIT;`,
+		CREATE INDEX "menu_card_id_7cfd48a2" ON "menu" ("card_id");`,
 	},
 }
